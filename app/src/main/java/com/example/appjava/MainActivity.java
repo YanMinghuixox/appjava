be8c7815798db62923fa61ded861a54fbe6b5173
Minghui Yan <yanmhbilly@163.com>
2022-05-26 23:59:02 +0800

    [review] finish activity [run pass]

--- app/src/main/java/com/example/appjava/MainActivity.java
+++ app/src/main/java/com/example/appjava/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.bt1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
